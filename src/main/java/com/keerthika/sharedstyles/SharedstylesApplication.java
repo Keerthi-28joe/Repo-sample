bb6fbc218c99781a89f61e8bdc6f9ffb497504df
Keerthi-28joe <rechelravi@gmail.com>
2025-04-07 21:38:05 +0530

    check Push

--- src/main/java/com/keerthika/sharedstyles/SharedstylesApplication.java
+++ src/main/java/com/keerthika/sharedstyles/SharedstylesApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SharedstylesApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println("hello world");
 		SpringApplication.run(SharedstylesApplication.class, args);
 	}
 
